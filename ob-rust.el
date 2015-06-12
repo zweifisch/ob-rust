dd09a25f375f3ae8665bab1a68a5a5b4044222dd
Feng Zhou <zf.pascal@gmail.com>
2015-06-12 20:22:41 +0800

    skip blank lines and comments

--- ob-rust.el
+++ ob-rust.el
@@ -15,35 +15,18 @@
 ;;
 
 ;;; Code:
-(require 'org)
 (require 'ob)
 
 (defun org-babel-execute:rust (body params)
   (ob-rust-eval (ob-rust-prep body)))
 
 (defun ob-rust-eval (body)
-  (let* ((src-tmp (org-babel-temp-file "rust-"))
-         (output-tmp (org-babel-temp-file "rustc-")))
+  (let ((src-tmp (org-babel-temp-file "rust-"))
+        (output-tmp (org-babel-temp-file "rustc-")))
     (with-temp-file src-tmp (insert body))
-    (let ((result (shell-command-to-string
-                   (format "rustc -A dead_code -o %s %s && %s"
-                           output-tmp src-tmp output-tmp))))
-      (message (prin1-to-string result))
-      result)))
-
-(defun ob-rust-last-char ()
-    (when (not (thing-at-point 'char t))
-      (forward-char -1))
-    (thing-at-point 'char t))
-
-(defun ob-rust-trim-right ()
-  (while
-      (let ((cur (ob-rust-last-char)))
-        (or (string-equal "\n" cur)
-            (string-equal "\r" cur)
-            (string-equal " " cur)
-            (string-equal ";" cur)))
-    (delete-char 1)))
+    (shell-command-to-string
+     (format "rustc -A dead_code -o %s %s && %s"
+             output-tmp src-tmp output-tmp))))
 
 (defun ob-rust-prep (body)
   (with-current-buffer (get-buffer-create "*ob-rust-src*")
@@ -51,12 +34,14 @@
     (insert "fn main() {\n")
     (insert body)
     (goto-char (point-max))
-    (ob-rust-trim-right)
     (beginning-of-line)
-    (if (or (string= "println" (thing-at-point 'word t))
-            (string= "}" (thing-at-point 'char t)))
+    (while (looking-at "\\(^[\t ]*//\\|^[\t ]*$\\)")
+      (forward-line -1))
+    (if (looking-at "[\t ]*\\(println\\|}\\)")
         (end-of-line)
       (insert "println!(\"{:?}\", ")
+      (when (search-forward-regexp ";[\t ]*$" nil t)
+        (replace-match "" t t))
       (end-of-line)
       (insert ");"))
     (insert "\n}")
